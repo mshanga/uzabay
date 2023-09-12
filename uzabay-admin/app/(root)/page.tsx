25bef4718058e39b8c1e036d3da32a354bb36937
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-09-12 13:38:09 +0300

    clerk auth setup

--- uzabay-admin/app/(root)/page.tsx
+++ uzabay-admin/app/(root)/page.tsx
@@ -1,6 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
-    <h1>Hello, NextJS!</h1>
-  )
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
+
+export default SetupPage;
